b30af21d6032ad35212b406a0bf8d9162d6eff8e
Kaden King <kingk45@ufl.edu>
2018-08-31 21:47:50 -0400

    working well

--- app/src/main/java/mo/bioinf/bmark/ResultsActivity.java
+++ app/src/main/java/mo/bioinf/bmark/ResultsActivity.java
@@ -1,5 +1,6 @@
 package mo.bioinf.bmark;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class ResultsActivity extends AppCompatActivity {
 
     @Override
@@ -17,13 +21,37 @@ public class ResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_results);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final Context context = this;
 
-        final Button done_button = (Button) findViewById(R.id.done_button);
+        final Button done_button = (Button) findViewById(R.id.finished_button);
         final TextView tv = (TextView) findViewById(R.id.time_text);
+        final TextView results_view = (TextView) findViewById(R.id.histogram);
 
         //set results text
         String results = getIntent().getStringExtra("runtime");
         tv.setText(results);
+
+        final String base_path = context.getFilesDir().getAbsolutePath().toString();
+        File histo = new File(base_path + "/../files.histo");
+//        if(histo.exists() && histo.canRead())
+//        {
+            try{
+                Scanner scanner = new Scanner(histo);
+
+                while(scanner.hasNextLine())
+                {
+                    results_view.append(scanner.nextLine() + "\n");
+                }
+
+
+            }catch(java.io.FileNotFoundException e)
+            {
+                results_view.setText("exception");
+            }
+//
+//        }else{
+//            results_view.setText("could not open");
+//        }
 
 
         done_button.setOnClickListener(new View.OnClickListener(){
